baaca208d3d8a3b026780a0732b55bebc55647d7
Febri Milansyah <febrimilan00@gmail.com>
2023-01-20 20:05:00 +0700

    Update Footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,217 @@
-!function(e){"use strict";e(".js-preloader").preloadinator({animation:"fadeOut",animationDuration:400});var t=e(".grid").isotope({});t.imagesLoaded().progress(function(){t.isotope("layout")}),e(".filter-button-group").on("click","button",function(){var a=e(this).attr("data-filter");t.isotope({filter:a})});for(var a=document.querySelectorAll(".button-group"),r=0,o=a.length;r<o;r++)i(a[r]);function i(e){e.addEventListener("click",function(t){matchesSelector(t.target,"button")&&(e.querySelector(".active").classList.remove("active"),t.target.classList.add("active"))})}new Swiper(".swiper",{breakpoints:{1200:{slidesPerView:3},992:{slidesPerView:2},576:{slidesPerView:1}},spaceBetween:24,loop:!0,autoplay:{delay:5e3},pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".partnerCarousel",{breakpoints:{1200:{slidesPerView:6},992:{slidesPerView:4},576:{slidesPerView:3},320:{slidesPerView:2}},spaceBetween:24,loop:!0,autoplay:{delay:5e3}});var n=L.map("mapwrapper").setView([-37.81716,144.955937],12);L.tileLayer("../../../../%7bs%7d.tile.openstreetmap.org/%7bz%7d/%7bx%7d/%7by%7d.png",{attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(n);var s=L.icon({iconUrl:"image/location.png",iconSize:[48,48]});L.marker([-37.81716,144.955937],{icon:s}).addTo(n),e(document).ready(function(){e(".site-header nav");var t=e(".hero-area").height();document.onscroll=function a(){e(window).scrollTop()>=t?e(".site-header nav").addClass("fixed-top"):e(".site-header nav").removeClass("fixed-top")}}),e(function(){var t=e("#contact-form"),a=e(".form-message p");e(t).submit(function(r){r.preventDefault();var o=e(t).serialize();e.ajax({type:"POST",url:e(t).attr("action"),data:o}).done(function(t){e(a).removeClass("error"),e(a).addClass("success"),e(a).text(t),e("#inputName").val(""),e("#inputEmail").val(""),e("#inputPhone").val(""),e("#inputMessage").val("")}).fail(function(t){e(a).removeClass("success"),e(a).addClass("error"),""!==t.responseText?e(a).text(t.responseText):e(a).text("Oops! An error occured and your message could not be sent.")})})}),e(".counter").counterUp({delay:10,time:3e3}),new Waypoint({element:document.getElementById("skill-section"),handler:function(){e(".progress .progress-bar").css("width",function(){return e(this).attr("aria-valuenow")+"%"})},offset:700}),AOS.init({once:!0})}(jQuery);
+(function($) { 
+    "use strict";
+    
+    
+    /*================================================================= 
+        Isotope initialization 
+    ==================================================================*/
+    var $grid = $('.grid').isotope({
+      // options
+    });
+    // layout Isotope after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.isotope('layout');
+    }); 
+    
+    // filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    });
+    
+    /* checking active filter */
+    // change is-checked class on buttons
+    var buttonGroups = document.querySelectorAll('.button-group');
+    for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup( buttonGroup );
+    }
+    
+    function radioButtonGroup( buttonGroup ) {
+    buttonGroup.addEventListener( 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    buttonGroup.querySelector('.active').classList.remove('active');
+    event.target.classList.add('active');
+    });
+    }
+    
+    
+    /*================================================================= 
+        Testimonial carousel
+    ==================================================================*/
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      breakpoints: {
+        1200:{
+          slidesPerView: 3,
+        },
+        992:{
+          slidesPerView: 2, 
+        },
+        576:{
+          slidesPerView: 1
+        },
+       },
+      //slidesPerView: 3,
+      spaceBetween: 24,
+      loop: true,
+      autoplay: {
+         delay: 5000,
+       },
+       
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+    
+    });
+    
+    
+    /*================================================================= 
+        Partner carousel
+    ==================================================================*/
+    const swiper2 = new Swiper('.partnerCarousel', {
+      // Optional parameters
+      breakpoints: {
+        1200:{
+          slidesPerView: 6,
+        },
+        992:{
+          slidesPerView: 4, 
+        },
+        576:{
+          slidesPerView: 3
+        },
+        320:{
+          slidesPerView: 2
+        },
+       },
+      //slidesPerView: 6,
+      spaceBetween: 24,
+      loop: true,
+      autoplay: {
+         delay: 5000,
+       },
+    
+    });
+    
+    
+    
+    /*================================================================= 
+        Navbar fixed top
+    ==================================================================*/
+    $(document).ready(function () {
+    
+        var menu = $('.site-header nav');
+        var origOffsetY = $('.hero-area').height();
+    
+        function scroll() {
+            if ($(window).scrollTop() >= origOffsetY) {
+                $('.site-header nav').addClass('fixed-top');
+                
+            } else {
+                $('.site-header nav').removeClass('fixed-top');
+               
+            }
+        }
+    
+        document.onscroll = scroll;
+    
+    });
+    
+    
+    /*================================================================= 
+        Contact form 
+    ==================================================================*/
+    $(function() {
+        // Here is the form
+        var form = $('#fungi-contact');
+    
+        // Getting the messages div
+        var formMessages = $('.form-message p');
+    
+    
+        // Setting up an event listener for the contact form
+      $(form).submit(function(event) {
+          // Stopping the browser to submit the form
+          event.preventDefault();
+          
+          // Serializing the form data
+        var formData = $(form).serialize();
+    
+        // Submitting the form using AJAX
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        }).done(function(response) {
+          
+            // Making the formMessages div to have the 'success' class
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+    
+            // Setting the message text
+            $(formMessages).text(response);
+    
+            // Clearing the form after successful submission 
+            $('#inputName').val('');
+            $('#inputEmail').val('');
+            $('#inputPhone').val('');
+            $('#inputMessage').val('');
+        }).fail(function(data) {
+          
+            // Making the formMessages div to have the 'error' class
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+    
+            // Setting the message text
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
+    
+      });
+    
+    });
+    
+    /*================================================================= 
+        Animating numbers
+    ==================================================================*/
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+    
+    
+    /*================================================================= 
+        Progress bar animation
+    ==================================================================*/
+    var waypoint = new Waypoint({
+      element: document.getElementById('skill-section'),
+      handler: function() {
+        $('.progress .progress-bar').css("width",function() {
+          return $(this).attr("aria-valuenow") + "%";
+      })
+      },
+      //offset: 'bottom-in-view',
+      offset: 700,
+    })
+    
+    
+    /*================================================================= 
+        Animate on scroll initialization
+    ==================================================================*/
+    AOS.init({
+      once: true,
+    });
+    
+    })(jQuery);
+    
+    
